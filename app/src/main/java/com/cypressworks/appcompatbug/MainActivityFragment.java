1fc647c0d6e2b78c790a94643d4a69af945aeaca
Kirill Rakhman <kirill.rakhman@gmail.com>
2015-04-23 15:21:46 +0200

    add SDK version to layout

--- app/src/main/java/com/cypressworks/appcompatbug/MainActivityFragment.java
+++ app/src/main/java/com/cypressworks/appcompatbug/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.cypressworks.appcompatbug;
 
-import android.support.v4.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,5 +21,12 @@ public class MainActivityFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
 //        return LayoutInflater.from(getActivity()).inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText("Android SDK: " + Build.VERSION.SDK_INT);
     }
 }
